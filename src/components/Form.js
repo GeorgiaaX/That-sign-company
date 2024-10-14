162aad3b2f290b81281e482dfe73572332f40299
GeorgiaaX <georgiaa.devv@gmail.com>
2024-10-14 13:30:43 +1000

    email update

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,7 @@ const ContactForm = React.forwardRef((props, ref) => {
       <Form
         name="contact"
         className="contact-form"
-        action="https://formsubmit.co/jake@thatsigncompany.com.au"
+        action="https://formsubmit.co/gsim17@hotmail.com"
         method="POST"
       >
         <h3 className="text-center">Send us a message</h3>
