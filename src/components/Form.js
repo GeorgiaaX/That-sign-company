da21852152a25e7e0d93f8ed2a376215b1c0c7b6
GeorgiaaX <georgiaa.devv@gmail.com>
2024-10-15 14:11:47 +1000

    changed form providers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,82 +1,3 @@
-// import React from "react";
-// import { Form, Button } from "react-bootstrap";
-
-// const ContactForm = React.forwardRef((props, ref) => {
-//   return (
-//     <div ref={ref}>
-//       <Form
-//         name="contact"
-//         className="contact-form"
-//         action="https://formsubmit.co/gsim17@hotmail.com"
-//         method="POST"
-//       >
-//         <h3 className="text-center">Send us a message</h3>
-//         {/* honeyPot */}
-//         <input type="text" name="_honey" style={{ display: "none" }} />
-//         {/* Disable captcha */}
-//         <input type="hidden" name="_captcha" value="false" />
-
-//         {/* Success Page */}
-//         <input
-//           type="hidden"
-//           name="_next"
-//           value="https://thatsigncompany.com.au/success"
-//         ></input>
-
-//         <Form.Group className="mb-3">
-//           <Form.Label className="contact-label" style={{ textAlign: "left" }}>
-//             Name
-//           </Form.Label>
-//           <Form.Control type="text" placeholder="Name" name="name" required />
-//         </Form.Group>
-
-//         <Form.Group className="mb-3">
-//           <Form.Label className="contact-form-label">Phone Number</Form.Label>
-//           <Form.Control
-//             type="tel"
-//             placeholder="Phone&nbsp;Number"
-//             name="phone number"
-//             required
-//           />
-//         </Form.Group>
-
-//         <Form.Group className="mb-3">
-//           <Form.Label className="contact-form-label">Email Address</Form.Label>
-//           <Form.Control
-//             type="email"
-//             placeholder="Email Address"
-//             name="email"
-//             required
-//           />
-//         </Form.Group>
-
-//         <Form.Group className="mb-3">
-//           <Form.Label className="contact-form-label">Message</Form.Label>
-//           <Form.Control
-//             as="textarea"
-//             placeholder="Tell us what you need!"
-//             name="message"
-//             rows="5"
-//             required
-//           />
-//         </Form.Group>
-
-//         <Button
-//           className="contact-btn"
-//           variant="primary"
-//           type="submit"
-//           size="lg"
-//         >
-//           Get a Quote
-//         </Button>
-//       </Form>
-//     </div>
-//   );
-// });
-
-// export default ContactForm;
-
-
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 
@@ -84,23 +5,21 @@ const ContactForm = React.forwardRef((props, ref) => {
   return (
     <div ref={ref}>
       <Form
-        name="contact" // Name your form
-        className="contact-form"
+        action="https://formspree.io/f/mvojyqvp" // Replace with your Formspree form ID
         method="POST"
-        data-netlify="true" 
-        netlify // Enable Netlify Forms processing
+        className="contact-form"
       >
         <h3 className="text-center">Send us a message</h3>
 
-        {/* Hidden field for Netlify form name */}
-        <input type="hidden" name="form-name" value="contact" />
-
-        {/* Success Page Redirection */}
+        {/* Hidden field for Formspree redirect */}
         <input
           type="hidden"
-          name="redirect"
-          value="https://thatsigncompany.com.au/success"
+          name="_next"
+          value="https://thatsigncompany.com.au/success" // Success page URL
         />
+
+        {/* Subject for the email */}
+        <input type="hidden" name="_subject" value="New Quote Request" />
 
         <Form.Group className="mb-3">
           <Form.Label className="contact-label" style={{ textAlign: "left" }}>
